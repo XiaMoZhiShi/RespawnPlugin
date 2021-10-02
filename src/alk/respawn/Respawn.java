7f37fd8d3acfa54ea2045eea5d4bd3aa19814d3d
ALKALiKong <alikongalk@gmail.com>
2021-10-02 19:32:32 +0800

    Add RespawnEventProcessor
    * Reduce the value of life when the player respawns
    * Switch player's gamemode to spectator when the value comes zero
    
    Signed-off-by: ALKALiKong <alikongalk@gmail.com>

--- src/alk/respawn/Respawn.java
+++ src/alk/respawn/Respawn.java
@@ -2,12 +2,7 @@ package alk.respawn;
 
 
 import org.bukkit.Bukkit;
-import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import java.util.EventListener;
 
 public class Respawn extends JavaPlugin {
     public static JavaPlugin instance;
@@ -18,5 +13,6 @@ public class Respawn extends JavaPlugin {
         instance = this;
         saveDefaultConfig();
         Bukkit.getPluginManager().registerEvents(new ConfigWriter(), this);
+        Bukkit.getPluginManager().registerEvents(new RespawnEventProcessor(), this);
     }
 }
