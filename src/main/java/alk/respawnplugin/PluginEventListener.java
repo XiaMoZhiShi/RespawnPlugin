aec466ba6b8789a45f9cd384d8eecfdc12af2eb3
ALKALiKong <alikongalk@gmail.com>
2022-01-17 21:41:41 +0800

    当玩家击杀小村民时有几率获得死亡轮回数
    Signed-off-by: ALKALiKong <alikongalk@gmail.com>

--- src/main/java/alk/respawnplugin/PluginEventListener.java
+++ src/main/java/alk/respawnplugin/PluginEventListener.java
@@ -3,9 +3,13 @@ package alk.respawnplugin;
 import com.destroystokyo.paper.event.player.PlayerSetSpawnEvent;
 import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.GameMode;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Villager;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
@@ -76,6 +80,28 @@ public class PluginEventListener extends PluginObject implements Listener {
         if (life_value == 1 || life_value == 0) {
             e.setKeepInventory(false);
             e.setDeathMessage("\uE464 " + e.getPlayer().getName() + " 死亡回归加护已耗尽，轮回结束");
+        }
+    }
+
+    @EventHandler
+    public void onPlayerKillVillagers(@NotNull EntityDeathEvent e){
+        Entity deathEntity = e.getEntity();
+        if (deathEntity.getType() == EntityType.VILLAGER){
+            Villager vi = (Villager) deathEntity;
+            if (vi.isAdult()){
+                //不要男妈妈不要男妈妈
+            } else {
+                //今日份迫害村民
+                if ( e.getEntity().getKiller() != null){
+                    if ( 0.2 < Math.random()){
+                        int lifevalue = Config.getInt(e.getEntity().getKiller().getName());
+                        int newValue = lifevalue + 1;
+                        Config.set(e.getEntity().getKiller().getName(), newValue);
+                        Plugin.saveConfig();
+                        e.getEntity().getKiller().sendMessage("\uE461 你的死亡回归加护次数 \uE46E + 1， 你当前剩余 \uE46E * " + newValue);
+                    }
+                }
+            }
         }
     }
 }
